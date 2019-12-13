60d980c657f19046d8dbbf1b84e6c7c99e82c119
Gilberto Ayala <gilbertoayala12@gmail.com>
2019-12-13 14:29:58 -0700

    Single photo component finished and created the comments component

---
+++ client/components/Comments.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render() {
+    return <div className="comments">Im the comments</div>;
+  }
+});
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
+  // index of the post
+  // get us the post itself
   render() {
-    return <div className="single-photo">I'm the single</div>;
+    const i = this.props.post.findIndex(
+      (post) => post.code === this.props.params.postId
+    );
+    const post = this.props.post[i];
+
+    return (
+      <div className="single-photo">
+        <Photo {...this.props} i={i} post={post} />
+        <Comments />
+      </div>
+    );
   }
 });
 export default Single;
